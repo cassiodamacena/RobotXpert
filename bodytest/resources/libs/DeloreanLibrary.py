255b7355f94b5fff8f785e923e6574d51f91d554
cassiodamacena <cassiodamacena@hotmail.com>
2021-04-03 18:19:31 -0300

    Refatorando Lib DeloreanLibrary.py ajustando conexão de BD nas métodos

--- bodytest/resources/libs/DeloreanLibrary.py
+++ bodytest/resources/libs/DeloreanLibrary.py
@@ -2,6 +2,15 @@ import psycopg2
 from logging import info
 
 class DeloreanLibrary():
+
+    def connect(self):
+        return psycopg2.connect(
+            host='ec2-23-21-229-200.compute-1.amazonaws.com',
+            database='dai6l32g2tis5l',
+            user='wdxdpikabhsxim',
+            password='2c5a8af0623ca9a329e474046cd2eebed8673aa22d9b444cce2e2d620a7e5937'
+        )
+
     
     # Metodos Python são reconhecidos como KW automagicamente pelo Robot
     # remove student email@desejado.com
@@ -10,12 +19,7 @@ class DeloreanLibrary():
         query = "delete from students where email  = '{}'".format(email)
         info(query)
 
-        conn = psycopg2.connect(
-            host='ec2-23-21-229-200.compute-1.amazonaws.com',
-            database='dai6l32g2tis5l',
-            user='wdxdpikabhsxim',
-            password='2c5a8af0623ca9a329e474046cd2eebed8673aa22d9b444cce2e2d620a7e5937'
-        )
+        conn = self.connect()
 
         cur = conn.cursor()
         cur.execute(query)
@@ -30,13 +34,9 @@ class DeloreanLibrary():
         query = ("insert into students (name, email, age, weight, feet_tall, created_at, updated_at) "
                 "values('{}','{}',{},{}, 1.75, now(), now());"
                 .format(student['name'], student['email'], student['age'], student['weight'], student['feet_tall']))
-        
-        conn = psycopg2.connect(
-            host='ec2-23-21-229-200.compute-1.amazonaws.com',
-            database='dai6l32g2tis5l',
-            user='wdxdpikabhsxim',
-            password='2c5a8af0623ca9a329e474046cd2eebed8673aa22d9b444cce2e2d620a7e5937'
-        )
+        info(query)
+
+        conn = self.connect()
 
         cur = conn.cursor()
         cur.execute(query)
